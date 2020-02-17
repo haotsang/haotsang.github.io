cb9b5694387f41cd2fccb9f4dbc1cb3e09d5d6f6
haotsang <1906993601@qq.com>
2020-02-18 00:57:03 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+config = {
+    blog_name: "haotsang的博客",
+    github_username: "haotsang", // github 账号名
+    github_repo: "haotsang.github.io",      // github 建立的 repo 名
+    per_page: 10,             // 首页每次载入文章列表数量
+
+    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // 将配置好的 access token 拆分成两个字符串相加
+    //access_token: "dbfe3dd66ee904988f2b"+"aaaf9bf49a03b6e3fd62"
+};
