d5a357fc38aadb4c3990cb119b83e3c90befa241
haotsang <1906993601@qq.com>
2023-02-17 19:50:00 +0800

    Reinitialize

---
+++ config.js
@@ -0,0 +1,10 @@
+config = {
+    blog_name: "HaoTsang's Blog",
+    github_username: "haotsang", // github 账号名
+    github_repo: "haotsang.github.io",      // github 建立的 repo 名
+    per_page: 10,             // 首页每次载入文章列表数量
+
+    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // 将配置好的 access token 拆分成两个字符串相加
+    access_token: "8e7165699f12da2bc0afd"+"141498e6edda3b0813c"
+};
