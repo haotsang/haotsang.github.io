4d1e7ebb75f9b3798feae3cd605012f594ebaef8
haotsang <1906993601@qq.com>
2023-02-17 20:01:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ config = {
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     // 将配置好的 access token 拆分成两个字符串相加
-    access_token: "8e7165699f12da2bc0afd"+"141498e6edda3b0813c"
+    access_token: "ghp_zmnJsxTWa7dqur" + "m4DOuaUEPaD3oJgM4BSo3g"
 };
